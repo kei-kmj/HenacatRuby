072cbc63f326df91a181f57cbff8e9d9f8e45f40
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-07-26 09:50:40 +0900

    complete the static web server

--- FlimsyServer/server_thread.rb
+++ FlimsyServer/server_thread.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
-class ServerThread
-  DOCUMENT_ROOT = '/mnt/c/mywebsite'
-  EXTENSION_TO_MIME =
-    {
-      'html' => 'text/html',
-      'txt' => 'text/plain',
-      'png' => 'image/png',
-      'jpg' => 'image/jpg',
-      'jpeg' => 'image/jpeg',
-      'gif' => 'image/gif',
-      'css' => 'text/css',
-      'js' => 'text/javascript'
-    }.freeze
+require_relative 'response_generator'
+require 'pathname'
+require 'cgi'
 
+class ServerThread
   def initialize(client)
     @client = client
     @path = nil
@@ -21,7 +12,7 @@ class ServerThread
 
   def process_request
     recv
-    send
+    ResponseGenerator.new(@client, @path).send_response
     close
   end
 
@@ -31,23 +22,7 @@ class ServerThread
     request = @client.gets
     @path = request.split[1] if request.start_with?('GET')
     request = @client.gets until request.chomp.empty?
-  end
-
-  def send
-    @client.puts("HTTP/1.0 200 OK\r\n")
-    @client.puts("Date: #{Time.now.gmtime.strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n")
-    @client.puts("Server: Modoki/0.1\r\n")
-    @client.puts("Content-Type: #{content_type}\r\n")
-    @client.puts("\r\n")
-
-    File.open("#{DOCUMENT_ROOT}#{@path}", 'r') do |f|
-      f.each_line { |line| @client.puts(line) }
-    end
-  end
-
-  def content_type
-    ext = @path.split('.')[-1]
-    EXTENSION_TO_MIME[ext] || 'text/plain'
+    @path = CGI.unescape(@path)
   end
 
   def close
